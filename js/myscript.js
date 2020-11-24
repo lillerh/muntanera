0fad0c9e77b48456829157f07df22b5a061936f5
Lille Reyes <lille@Lilles-MacBook-Pro.local>
2020-11-24 14:49:40 +0100

    Initial commit - background video working WIP

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+var muntaneraText = document.querySelector(".muntanera-text");
+
+document.body.addEventListener("scroll", function(event) {
+  var opacity = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
+  var scale = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
+  document.documentElement.style.setProperty('--headerOpacity', opacity);
+  document.documentElement.style.setProperty('--headerScale', scale);
+});
+
